999c656476d0ea07e7482a2728e90c5504da7145
Guru Swaroop <bguruswaroop@gmail.com>
2020-12-07 12:27:54 +0100

    patch aggregation - majority (predictions), weighted & log weighted (classification scores)

--- run_flow.py
+++ run_flow.py
@@ -1,9 +1,12 @@
 import logging
+import pickle
 
 from configure import Configure, SigNet, SimNet
 from signature_net.sig_net_flow import SigNetFlow
 from similarity_net.sim_net_flow import SimNetFlow
+from utils.evaluation_metrics import MultinomialClassificationScores
 from utils.logging import SetupLogger, log_running_time
+from utils.visualization_utils import VisualizationUtils
 
 logger = logging.getLogger(__name__)
 import argparse
@@ -18,12 +21,12 @@ def run_flow(fold_id):
 
     # Signature Net
     Configure.sig_net_name = f'signature_net_{fold_id}'
-    Configure.train_data = rf'/data/p288722/dresden/train/nat_patches_18_models_128x128_15/fold_{fold_id}.json'
-    Configure.test_data = rf'/data/p288722/dresden/test/nat_patches_18_models_128x128_15/fold_{fold_id}.json'
-    Configure.create_runtime_dirs()
+    Configure.update()
 
     SigNetFlow.train()
-    SigNetFlow.classify()
+    SigNetFlow.classify(aggregation_method='majority_vote')
+    SigNetFlow.classify(aggregation_method='prediction_score_sum')
+    SigNetFlow.classify(aggregation_method='log_scaled_prediction_score_sum')
 
     # Utils.visualize_ae_input_output_pairs()
     # Utils.save_avg_fourier_images()
@@ -50,7 +53,7 @@ def run_cross_validation_flow():
         Configure.train_data = rf'/data/p288722/dresden/train/nat_patches_18_models_128x128_15/fold_{fold_id}.json'
         Configure.test_data = rf'/data/p288722/dresden/test/nat_patches_18_models_128x128_15/fold_{fold_id}.json'
         Configure.sig_net_name = f'signature_net_{fold_id}'
-        Configure.create_runtime_dirs()
+        Configure.update()
 
         SigNetFlow.train()
         SigNetFlow.classify()
@@ -59,7 +62,7 @@ def run_cross_validation_flow():
         Configure.train_data = rf'/data/p288722/dresden/train/nat_patches_18_models_128x128_3/fold_{fold_id}.json'
         Configure.test_data = rf'/data/p288722/dresden/test/nat_patches_18_models_128x128_3/fold_{fold_id}.json'
         Configure.sim_net_name = f'similarity_net_{fold_id}'
-        Configure.create_runtime_dirs()
+        Configure.update()
 
         SimNet.balance_classes = True
         SimNetFlow.train()
@@ -67,22 +70,190 @@ def run_cross_validation_flow():
         SimNetFlow.classify()
 
 
+def run_flow_hierarchial():
+    import json
+    from pathlib import Path
+    import datetime
+    import shutil
+
+    temp_dir = Configure.runtime_dir.joinpath('temp')
+    if temp_dir.exists():
+        shutil.rmtree(temp_dir)
+    temp_dir.mkdir(exist_ok=True, parents=True)
+
+    # Step 1 : Brand level training and classification
+    def create_brands_dataset_from_models(filename):
+        with open(filename, 'r') as f:
+            models_dict = json.load(f)['file_paths']
+        brands_dict = {}
+        for model_name in sorted(models_dict.keys()):
+            brand_name = model_name.split('_')[0]
+            if brand_name not in brands_dict:
+                brands_dict[brand_name] = []
+            brands_dict[brand_name].extend(models_dict[model_name])
+        timestamp = str(datetime.datetime.now()).replace(' ', '').replace('-', '').replace(':', '').replace('.', '')
+        modified_json_file = temp_dir.joinpath(f'{timestamp}_brands_{Path(filename).name}')
+        with open(modified_json_file, 'w+') as f:
+            json.dump({'file_paths': brands_dict}, f, indent=2)
+        return modified_json_file
+
+    source_train_data, source_test_data = Configure.train_data, Configure.test_data
+    Configure.train_data = create_brands_dataset_from_models(source_train_data)
+    Configure.test_data = create_brands_dataset_from_models(source_test_data)
+
+    with open(Configure.train_data, 'r') as f:
+        brands_dataset = json.load(f)['file_paths']
+        num_classes = len(brands_dataset)
+        SigNet.update_model(num_classes, is_constrained=False)
+
+    Configure.sig_net_name = f'signature_net_brands'
+    Configure.update()
+
+    SigNetFlow.train()
+    SigNetFlow.classify()
+
+    # Step 2: model level training and classification
+    def create_multi_models_datasets_from_models_dataset(filename):
+        with open(filename, 'r') as f:
+            models_dict = json.load(f)['file_paths']
+        brands_dict = {}
+        for model_name in sorted(models_dict.keys()):
+            brand_name = model_name.split('_')[0]
+            if brand_name not in brands_dict:
+                brands_dict[brand_name] = {model_name: models_dict[model_name]}
+            else:
+                brands_dict[brand_name][model_name] = models_dict[model_name]
+
+        modified_files = []
+        for brand_name in brands_dict:
+            if len(brands_dict[brand_name]) > 1:
+                timestamp = str(datetime.datetime.now()).replace(' ', '').replace('-', '').replace(':', '').replace('.',
+                                                                                                                    '')
+                modified_json_file = temp_dir.joinpath(f'{timestamp}_{brand_name}_{Path(filename).name}')
+                with open(modified_json_file, 'w+') as f:
+                    json.dump({'file_paths': brands_dict[brand_name]}, f, indent=2)
+                modified_files.append(modified_json_file)
+
+        return modified_files
+
+    train_data_models = create_multi_models_datasets_from_models_dataset(source_train_data)
+    test_data_models = create_multi_models_datasets_from_models_dataset(source_test_data)
+
+    camera_model_ids = {}
+    camera_brand_names = {brand_name: idx for idx, brand_name in enumerate(sorted(brands_dataset.keys()))}
+    camera_brand_ids = {idx: brand_name for idx, brand_name in enumerate(sorted(brands_dataset.keys()))}
+
+    for train_data, test_data in zip(train_data_models, test_data_models):
+        Configure.train_data = train_data
+        Configure.test_data = test_data
+        with open(Configure.train_data, 'r') as f:
+            num_classes = len(json.load(f)['file_paths'])
+        SigNet.update_model(num_classes, is_constrained=False)
+
+        brand_name = train_data.stem.split("_")[1]
+        Configure.sig_net_name = f'signature_net_{brand_name}'
+        camera_model_ids[camera_brand_names[brand_name]] = Configure.sig_net_name
+        Configure.update()
+
+        SigNetFlow.train()
+        SigNetFlow.classify()
+
+    # Classify at brand level
+    Configure.sig_net_name = f'signature_net_brands'
+    Configure.train_data = create_brands_dataset_from_models(source_train_data)
+    Configure.test_data = create_brands_dataset_from_models(source_test_data)
+    Configure.update()
+    with open(Configure.train_data, 'r') as f:
+        num_classes = len(json.load(f)['file_paths'])
+    SigNet.update_model(num_classes, is_constrained=False)
+    SigNet.model.eval()
+    image_paths, brand_predictions, brand_pred_scores = SigNetFlow.predict(Configure.test_data)
+
+    import numpy as np
+    model_predictions = np.zeros_like(brand_predictions)
+    for idx, brand_id in enumerate(camera_model_ids):
+        indices = np.where(brand_predictions == brand_id)[0]
+        if len(indices) > 0:
+            filtered_image_paths = np.array(image_paths)[indices.astype(int)]
+            Configure.sig_net_name = f'signature_net_{camera_brand_ids[brand_id]}'
+            Configure.test_data = test_data_models[idx]
+            Configure.train_data = train_data_models[idx]
+            Configure.update()
+            with open(Configure.train_data, 'r') as f:
+                num_classes = len(json.load(f)['file_paths'])
+            SigNet.update_model(num_classes, is_constrained=False)
+            filtered_image_paths = [str(x) for x in filtered_image_paths]
+            SigNet.model.eval()
+            _, predictions, pred_scores = SigNetFlow.predict(filtered_image_paths)
+            for idx, item in zip(indices, predictions):
+                model_predictions[idx] = item
+
+    # Merge brand and model level predictions
+    predictions = brand_predictions * 10 + model_predictions
+    labels_correction_map = {pr: idx for idx, pr in
+                             enumerate(sorted(set(predictions)))}  # this kind of labelling is misl
+    predictions = [labels_correction_map[x] for x in predictions]
+
+    model_path_to_gt_dict = {}
+
+    with open(source_test_data, 'r') as f:
+        test_data_dict = json.load(f)['file_paths']
+        model_name_to_gt_label = {model_name: idx for idx, model_name in enumerate(sorted(test_data_dict.keys()))}
+        for model_name in test_data_dict.keys():
+            for path in test_data_dict[model_name]:
+                model_path_to_gt_dict[path] = model_name_to_gt_label[model_name]
+
+    ground_truths = []
+    for path in image_paths:
+        ground_truths.append(model_path_to_gt_dict[path])
+
+    # fixme: predicted scores is not being passed correctly
+    ground_truths, predictions = SigNetFlow.patch_to_image(ground_truths, predictions, image_paths,
+                                                           aggregation_method='majority_vote')
+
+    # computing classification scores
+    scores = MultinomialClassificationScores(ground_truths, predictions, one_hot=False,
+                                             camera_names=sorted(test_data_dict.keys()))
+    scores.log_scores()
+    with open(str(Configure.signet_dir.joinpath('scores.pkl')), 'wb+') as f:
+        pickle.dump(scores, f)
+    VisualizationUtils.plot_confusion_matrix(ground_truths, predictions,
+                                             one_hot=False, save_to_dir=Configure.runtime_dir.joinpath('signature_net'))
+
+
+def run_flow_hierarchical_balanced():
+    pass
+
+
 if __name__ == '__main__':
     import inspect
+    from pathlib import Path
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-fold', type=str, help='enter fold number')
-    args = parser.parse_args()
-    fold_id = 1
+    parser.add_argument('-num_patches', type=str, help='enter num patches')
 
-    SetupLogger(log_file=Configure.runtime_dir.joinpath(f'scd_{fold_id}.log'))
+    args = parser.parse_args()
+    # fold_id = args.fold
+    # num_patches = args.num_patches
+    fold_id = 1
+    num_patches = 100
+
+    Configure.runtime_dir = Path(
+        rf'/scratch/p288722/runtime_data/scd_pytorch/18_models_hierarchial_4/fold_{fold_id}')
+    Configure.train_data = rf'/data/p288722/dresden/train/nat_patches_18_models_128x128_100/fold_{fold_id}.json'
+    Configure.test_data = rf'/data/p288722/dresden/test/nat_patches_18_models_128x128_100/fold_{fold_id}.json'
+    Configure.update()
+
+    SetupLogger(log_file=Configure.runtime_dir.joinpath(f'scd_{fold_id}_exp.log'))
     for item in [Configure, SigNet, SimNet]:
         logger.info(f'---------- {item.__name__} ----------')
         for name, value in inspect.getmembers(item)[26:]:
             logger.info(f'{name.ljust(20)}: {value}')
     try:
         # run_cross_validation_flow()
-        run_flow(fold_id)
+        # run_flow(fold_id)
+        run_flow_hierarchial()
     except Exception as e:
         logger.error(e)
         raise e
--- signature_net/sig_net_flow.py
+++ signature_net/sig_net_flow.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import pickle
+from collections import namedtuple
 from pathlib import Path
 from time import perf_counter
 
@@ -15,6 +16,8 @@ from utils.training_utils import Utils
 from utils.visualization_utils import VisualizationUtils
 
 logger = logging.getLogger(__name__)
+
+Stats = namedtuple('Stats', ('loss', 'acc', 'gt', 'pred', 'pred_scores'), defaults=(None, None, None, None, None))
 
 
 class SigNetFlow(object):
@@ -39,30 +42,23 @@ class SigNetFlow(object):
         ground_truths = torch.max(targets, dim=1).indices
         accuracy = torch.mean(torch.as_tensor(ground_truths == predictions, dtype=torch.float32))
 
-        return loss.item(), accuracy.item()
+        stats = Stats(loss=loss.item(), acc=accuracy.item())
+        return stats
 
     @staticmethod
     @torch.no_grad()
-    def test_batch(inputs, targets, return_predictions=False, device_to_model_map=None):
+    def test_batch(inputs, targets, device_to_model_map=None):
         outputs = SigNet.model(inputs)
-
-        if outputs.shape != targets.shape:
-            # Convert the outputs to model level predictions
-            model_level_outputs = torch.tensor(np.zeros(targets.shape), dtype=torch.float32).to(Configure.device)
-            for device_idx, model_idx in device_to_model_map.items():
-                model_level_outputs[:, model_idx] += outputs[:, device_idx]
-            outputs = model_level_outputs
-
         loss = SigNet.criterion(outputs, targets)
 
         predictions = torch.max(outputs, dim=1).indices
+        prediction_scores = torch.max(outputs, dim=1).values
         ground_truths = torch.max(targets, dim=1).indices
         accuracy = torch.mean(torch.as_tensor(ground_truths == predictions, dtype=torch.float32))
 
-        if return_predictions:
-            return loss.item(), accuracy.item(), ground_truths.cpu().numpy(), predictions.cpu().numpy()
-        else:
-            return loss.item(), accuracy.item()
+        stats = Stats(loss=loss.item(), acc=accuracy.item(), gt=ground_truths.cpu().numpy(),
+                      pred=predictions.cpu().numpy(), pred_scores=prediction_scores.cpu().numpy())
+        return stats
 
     @classmethod
     @log_running_time
@@ -80,7 +76,9 @@ class SigNetFlow(object):
         models_list = list(sorted(set([x[:-2] for x in devices_list])))
         device_to_model_map = {idx: models_list.index(x[:-2]) for idx, x in enumerate(devices_list)}
 
-        init_epoch, history, model = Utils.prepare_for_training(Configure.signet_dir, SigNet.model)
+        init_epoch, history, SigNet.model, SigNet.optimizer, SigNet.scheduler = Utils.prepare_for_training(
+            Configure.signet_dir, SigNet.model, SigNet.optimizer, SigNet.scheduler)
+
         for epoch in range(init_epoch, SigNet.epochs + 1):
             # Train
             SigNet.model.train()
@@ -89,7 +87,8 @@ class SigNetFlow(object):
 
             epoch_start_time = perf_counter()
             for i, (input_images, (target_labels, _)) in enumerate(train_loader):
-                loss[i], acc[i] = cls.train_batch(input_images.to(Configure.device), target_labels.to(Configure.device))
+                stats = cls.train_batch(input_images.to(Configure.device), target_labels.to(Configure.device))
+                loss[i], acc[i] = stats.loss, stats.acc
                 if i % 100 == 0:
                     logger.info(f'Running batch : {i}')
 
@@ -105,8 +104,9 @@ class SigNetFlow(object):
             loss, acc = np.zeros(num_batches), np.zeros(num_batches)
             logger.info(f'Running validate job')
             for i, (input_images, (target_labels, _)) in enumerate(test_loader):
-                loss[i], acc[i] = cls.test_batch(input_images.to(Configure.device), target_labels.to(Configure.device),
-                                                 device_to_model_map=device_to_model_map)
+                stats = cls.test_batch(input_images.to(Configure.device), target_labels.to(Configure.device),
+                                       device_to_model_map=device_to_model_map)
+                loss[i], acc[i] = stats.loss, stats.acc
 
             val_acc, val_loss = np.mean(acc), np.mean(loss)
 
@@ -116,7 +116,8 @@ class SigNetFlow(object):
             logger.info(f'plot lr')
             VisualizationUtils.plot_learning_statistics(history, Configure.signet_dir)
             logger.info(f'save')
-            Utils.save_model_on_epoch_end(SigNet.model, history, Configure.signet_dir)
+            Utils.save_model_on_epoch_end(SigNet.model.state_dict(), SigNet.optimizer.state_dict(),
+                                          SigNet.scheduler.state_dict(), history, Configure.signet_dir)
 
             logger.info(f"epoch : {epoch}/{SigNet.epochs}, "
                         f"train_loss = {train_loss:.6f}, val_loss = {val_loss:.6f}, "
@@ -140,7 +141,8 @@ class SigNetFlow(object):
 
         if not pre_trained_model_path:
             pre_trained_model_path = Configure.runtime_dir.joinpath('{}.pt'.format(SigNet.name))
-        SigNet.model = torch.load(pre_trained_model_path)
+        params = torch.load(pre_trained_model_path)
+        SigNet.model.load_state_dict(params['model_state_dict'])
 
         if config_mode == 'train' and not images_dir:
             data_loader = Data.load_data(dataset=Configure.train_data, config_mode=config_mode)
@@ -163,7 +165,7 @@ class SigNetFlow(object):
 
     @classmethod
     @log_running_time
-    def classify(cls, config_mode='test', pre_trained_model_path=None):
+    def classify(cls, config_mode='test', pre_trained_model_path=None, aggregation_method=None):
         """
         Method to extract signatures and labels
         :param config_mode: string - train / test
@@ -172,7 +174,9 @@ class SigNetFlow(object):
         """
         if not pre_trained_model_path:
             pre_trained_model_path = Configure.runtime_dir.joinpath('{}.pt'.format(SigNet.name))
-        SigNet.model = torch.load(pre_trained_model_path)
+        params = torch.load(pre_trained_model_path)
+        SigNet.model.load_state_dict(params['model_state_dict'])
+        SigNet.model.eval()
 
         if config_mode == 'train':
             data_loader = Data.load_data(dataset=Configure.train_data, config_mode=config_mode)
@@ -195,20 +199,28 @@ class SigNetFlow(object):
 
         num_batches = len(data_loader)
         loss, acc = np.zeros(num_batches), np.zeros(num_batches)
-        ground_truths, predictions = [None] * num_batches, [None] * num_batches
+        ground_truths, predictions, prediction_scores = [None] * num_batches, [None] * num_batches, [None] * num_batches
         image_paths = []
 
         for batch_id, (input_images, (target_labels, img_paths)) in enumerate(data_loader):
-            loss[batch_id], acc[batch_id], ground_truths[batch_id], predictions[batch_id] = \
-                cls.test_batch(input_images.to(Configure.device), target_labels.to(Configure.device),
-                               return_predictions=True, device_to_model_map=device_to_model_map)
+            stats = cls.test_batch(input_images.to(Configure.device), target_labels.to(Configure.device),
+                                   device_to_model_map)
+            loss[batch_id], acc[batch_id], ground_truths[batch_id], predictions[batch_id], prediction_scores[batch_id] = \
+                stats.loss, stats.acc, stats.gt, stats.pred, stats.pred_scores
             image_paths += img_paths
 
         logger.info(f'Test loss: {np.mean(loss)}')
         logger.info(f'Test accuracy: {np.mean(acc)}')
 
-        ground_truths, predictions = np.concatenate(ground_truths), np.concatenate(predictions)
-        ground_truths, predictions = SigNetFlow.patch_to_image(ground_truths, predictions, image_paths)
+        ground_truths, predictions, prediction_scores = \
+            np.concatenate(ground_truths), np.concatenate(predictions), np.concatenate(prediction_scores)
+
+        # aggregation_method = 'predictions_scores'
+        aggregation_method = 'majority_vote'
+
+        logger.info(f'Converting patch predictions to image predictions using {aggregation_method}')
+        ground_truths, predictions = SigNetFlow.patch_to_image(ground_truths, predictions, image_paths,
+                                                               aggregation_method, prediction_scores)
 
         scores = MultinomialClassificationScores(ground_truths, predictions, one_hot=False, camera_names=models_list)
         scores.log_scores()
@@ -238,8 +250,41 @@ class SigNetFlow(object):
             VisualizationUtils.plot_confusion_matrix(ground_truths, predictions, one_hot=False,
                                                      save_to_dir=results_dir)
 
+    @classmethod
+    @log_running_time
+    @torch.no_grad()
+    def predict(cls, prediction_data, pre_trained_model_path=None):
+        """
+        Method to extract signatures and labels
+        :param config_mode: string - train / test
+        :param pre_trained_model_path: (optional) Pre-trained model path
+        :return: list of labelled signatures
+        """
+        if not pre_trained_model_path:
+            pre_trained_model_path = Configure.runtime_dir.joinpath(f'{SigNet.name}.pt')
+        params = torch.load(pre_trained_model_path)
+        SigNet.model.load_state_dict(params['model_state_dict'])
+        SigNet.model.eval()
+        data_loader = Data.load_data(dataset=prediction_data, config_mode='test')
+
+        num_batches = len(data_loader)
+        predictions, prediction_scores = [None] * num_batches, [None] * num_batches
+        image_paths = []
+
+        for batch_id, (input_images, (target_labels, img_paths)) in enumerate(data_loader):
+            outputs = SigNet.model(input_images.to(Configure.device)).to(torch.device("cpu"))
+            predictions[batch_id] = torch.max(outputs, dim=1).indices
+            prediction_scores[batch_id] = torch.max(outputs, dim=1).values
+            image_paths += img_paths
+
+        predictions = np.concatenate(predictions)
+        prediction_scores = np.concatenate(prediction_scores)
+        _, predictions = SigNetFlow.patch_relabelling_by_aggregation(predictions, predictions, image_paths)
+
+        return image_paths, predictions, prediction_scores
+
     @staticmethod
-    def patch_to_image(ground_truths, predictions, image_paths, aggregation_method='majority_vote'):
+    def patch_relabelling_by_aggregation(ground_truths, predictions, image_paths, aggregation_method='majority_vote'):
         """
         Convert the patch level predictions to image level predictions.
         :param ground_truths:
@@ -251,6 +296,7 @@ class SigNetFlow(object):
 
         from collections import Counter
 
+        # Combine all the patches from an image and make a dictionary
         patches_per_image_dict = {}
         for patch_id, gt, pr in zip(image_paths, ground_truths, predictions):
             patch_id = Path(patch_id)
@@ -261,11 +307,84 @@ class SigNetFlow(object):
                 patches_per_image_dict[image_id]['gt'].append(gt)
                 patches_per_image_dict[image_id]['pr'].append(pr)
 
+        # Perform label aggregation, and save the result back in the dictionary
+        if aggregation_method == 'majority_vote':
+            majority_vote = lambda x: Counter(x).most_common(1)[0][0]
+            for idx, image_id in enumerate(patches_per_image_dict):
+                patches_per_image_dict[image_id]['gt'] = majority_vote(patches_per_image_dict[image_id]['gt'])
+                patches_per_image_dict[image_id]['pr'] = majority_vote(patches_per_image_dict[image_id]['pr'])
+        else:
+            raise ValueError('Invalid aggregation_method')
+
+        # Relabel the patches with the aggregated labels
+        for idx, (patch_id, gt, pr) in enumerate(zip(image_paths, ground_truths, predictions)):
+            patch_id = Path(patch_id)
+            image_id = '_'.join((patch_id.parent.name + '/' + patch_id.name).split('_')[:-1])
+            new_label = patches_per_image_dict[image_id]
+            ground_truths[idx] = new_label['gt']
+            predictions[idx] = new_label['pr']
+
+        return np.array(ground_truths), np.array(predictions)
+
+    @staticmethod
+    def patch_to_image(ground_truths, predictions, image_paths, aggregation_method, prediction_scores=None):
+        """
+        Convert the patch level predictions to image level predictions.
+        :param ground_truths:
+        :param predictions:
+        :param image_paths:
+        :param aggregation_method: a string with values 'majority_vote'
+        :return: None
+        """
+
+        from collections import Counter
+
+        if prediction_scores is None:
+            prediction_scores = np.ones_like(predictions)
+
+        patches_per_image_dict = {}
+        for patch_id, gt, pr, pr_s in zip(image_paths, ground_truths, predictions, prediction_scores):
+            patch_id = Path(patch_id)
+            image_id = '_'.join((patch_id.parent.name + '/' + patch_id.name).split('_')[:-1])
+            if image_id not in patches_per_image_dict:
+                patches_per_image_dict[image_id] = {'gt': [gt], 'pr': [pr], 'pr_s': [pr_s]}
+            else:
+                patches_per_image_dict[image_id]['gt'].append(gt)
+                patches_per_image_dict[image_id]['pr'].append(pr)
+                patches_per_image_dict[image_id]['pr_s'].append(pr_s)
+
         ground_truths, predictions = [], []
         if aggregation_method == 'majority_vote':
             for idx, image_id in enumerate(patches_per_image_dict):
                 ground_truths.append(Counter(patches_per_image_dict[image_id]['gt']).most_common(1)[0][0])
                 predictions.append(Counter(patches_per_image_dict[image_id]['pr']).most_common(1)[0][0])
+
+        elif aggregation_method == 'prediction_score_sum':
+            for idx, image_id in enumerate(patches_per_image_dict):
+                ground_truths.append(Counter(patches_per_image_dict[image_id]['gt']).most_common(1)[0][0])
+
+                image_prediction = {}
+                for pr, pr_s in zip(patches_per_image_dict[image_id]['pr'], patches_per_image_dict[image_id]['pr_s']):
+                    if pr not in image_prediction:
+                        image_prediction[pr] = pr_s
+                    else:
+                        image_prediction[pr] += pr_s
+                pred = max(image_prediction, key=lambda key: image_prediction[key])
+                predictions.append(pred)
+
+        elif aggregation_method == 'log_scaled_prediction_score_sum':
+            for idx, image_id in enumerate(patches_per_image_dict):
+                ground_truths.append(Counter(patches_per_image_dict[image_id]['gt']).most_common(1)[0][0])
+
+                image_prediction = {}
+                for pr, pr_s in zip(patches_per_image_dict[image_id]['pr'], patches_per_image_dict[image_id]['pr_s']):
+                    if pr not in image_prediction:
+                        image_prediction[pr] = -1.0 / np.log(pr_s)
+                    else:
+                        image_prediction[pr] += -1.0 / np.log(pr_s)
+                pred = max(image_prediction, key=lambda key: image_prediction[key])
+                predictions.append(pred)
+
         else:
             raise ValueError('Invalid aggregation_method')
 
